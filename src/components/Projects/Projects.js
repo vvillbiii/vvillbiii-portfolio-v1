378c8dc5d739224243d1d3cba3efd4208520fc87
vvillbii <will.bowles90@gmail.com>
2022-04-20 13:53:52 -0400

    updated css for project component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,57 +5,71 @@ import Conscious from "../../assets/Conscious Culture Reads.png";
 
 const Projects = () => {
   return (
-    <div className="project-section">
-      <h2>Projects</h2>
-      <div className="project">
-        <h3>Conscious Culture Reads</h3>
-        <img src={Conscious} alt="Conscious Culture Reads Image" />
-        <div className="project-content">
-          <p>
-            Conscious Culture Reads allows you explore article that has been
-            shared on the Conscious Culture newsletter and save them to your
-            profile, so you can always revisit your favorite reads anytime you
-            want. Conscious Culture Reads also let's you create notes on what
-            you read so you have a place to put your takeaways.
-          </p>
-          <button>Live Site</button>
-          <button>Github</button>
+    <div className="project-container">
+      <div className="project-section">
+        <h2>Projects</h2>
+        <div className="project-one">
+          <img src={Conscious} alt="Conscious Culture Reads" />
+          <div className="project-one__content">
+            <h3>Conscious Culture Reads</h3>
+            <p>
+              Conscious Culture Reads allows you explore article that has been
+              shared on the Conscious Culture newsletter and save them to your
+              profile, so you can always revisit your favorite reads anytime you
+              want. Conscious Culture Reads also let's you create notes on what
+              you read so you have a place to put your takeaways.
+            </p>
+            <div className="project-buttons">
+              <button className="live-link">Live Site</button>
+              <a href="https://github.com/vvillbiii/ccreads-frontend">
+                <button className="github-link">Github</button>
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="right">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl
-          eros, pulvinar facilisis justo mollis, auctor consequat urna. Morbi a
-          bibendum metus. Donec scelerisque sollicitudin enim eu venenatis. Duis
-          tincidunt laoreet ex, in pretium orci vestibulum eget. Class aptent
-          taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-          himenaeos. Duis pharetra luctus lacus ut vestibulum. Maecenas ipsum
-          lacus, lacinia quis posuere ut, pulvinar vitae dolor. Integer eu nibh
-          at nisi ullamcorper sagittis id ve
-        </p>
-        <button>Live Site</button>
-        <button>Github</button>
-        <h3>Nomad Atlas</h3>
-        <img src={Nomad} alt="Nomad Atlas Image" />
-      </div>
-      <div>
-        <div className="project-left">
-          <h3>AlimenT</h3>
-          <img src={Aliment} alt="Aliment Image" />
+        <div className="project-two">
+          <img src={Nomad} alt="Nomad Atlas" />
+          <div className="project-two__content">
+            <h3>Nomad Atlas</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+              nisl eros, pulvinar facilisis justo mollis, auctor consequat urna.
+              Morbi a bibendum metus. Donec scelerisque sollicitudin enim eu
+              venenatis. Duis tincidunt laoreet ex, in pretium orci vestibulum
+              eget. Class aptent taciti sociosqu ad litora torquent per conubia
+              nostra, per inceptos himenaeos. Duis pharetra luctus lacus ut
+              vestibulum. Maecenas ipsum lacus, lacinia quis posuere ut,
+              pulvinar vitae dolor. Integer eu nibh at nisi ullamcorper sagittis
+              id ve
+            </p>
+            <div className="project-buttons">
+              <button className="live-link">Live Site</button>
+              <button className="github-link">Github</button>
+            </div>
+          </div>
         </div>
-        <div className="project-right">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-            nisl eros, pulvinar facilisis justo mollis, auctor consequat urna.
-            Morbi a bibendum metus. Donec scelerisque sollicitudin enim eu
-            venenatis. Duis tincidunt laoreet ex, in pretium orci vestibulum
-            eget. Class aptent taciti sociosqu ad litora torquent per conubia
-            nostra, per inceptos himenaeos. Duis pharetra luctus lacus ut
-            vestibulum. Maecenas ipsum lacus, lacinia quis posuere ut, pulvinar
-            vitae dolor. Integer eu nibh at nisi ullamcorper sagittis id ve
-          </p>
-          <button>Live Site</button>
-          <button>Github</button>
+        <div>
+          <div className="project-three">
+            <img src={Aliment} alt="Aliment" />
+            <div className="project-three__content">
+              <h3>AlimenT</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                nisl eros, pulvinar facilisis justo mollis, auctor consequat
+                urna. Morbi a bibendum metus. Donec scelerisque sollicitudin
+                enim eu venenatis. Duis tincidunt laoreet ex, in pretium orci
+                vestibulum eget. Class aptent taciti sociosqu ad litora torquent
+                per conubia nostra, per inceptos himenaeos. Duis pharetra luctus
+                lacus ut vestibulum. Maecenas ipsum lacus, lacinia quis posuere
+                ut, pulvinar vitae dolor. Integer eu nibh at nisi ullamcorper
+                sagittis id ve
+              </p>
+              <div className="project-buttons">
+                <button className="live-link">Live Site</button>
+                <button className="github-link">Github</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
