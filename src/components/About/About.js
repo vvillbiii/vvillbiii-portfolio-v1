6a1e7438192cca5307facdf64a83f316edec903e
vvillbii <will.bowles90@gmail.com>
2022-04-20 16:17:14 -0400

    updated media query to target max-width 644px

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,15 +6,17 @@ const About = () => {
       <div className="about-section">
         <div>
           <h2>About Me</h2>
-          <p>
-            I'm software engineer with a background in SEO and digital
-            marketing. Finding new ways to solve problems and learning new
-            skills are interests of mine.
-            <br /> In addition to solving problems independently, I value
-            collaboration because there is more than one way to achieve an
-            answer, and everyone’s input is valuable. My strengths are my
-            attention to detail and my ability to meet deadlines.
-          </p>
+          <div className="about-section__content">
+            <p>
+              I'm software engineer with a background in SEO and digital
+              marketing. Finding new ways to solve problems and learning new
+              skills are interests of mine.
+              <br /> In addition to solving problems independently, I value
+              collaboration because there is more than one way to achieve an
+              answer, and everyone’s input is valuable. My strengths are my
+              attention to detail and my ability to meet deadlines.
+            </p>
+          </div>
         </div>
         <div className="tech-section">
           <h2>Technologies I Work With</h2>
