c579195b9de650f85e2d0359b8c0c479a0689fc9
vvillbii <will.bowles90@gmail.com>
2022-04-20 14:04:30 -0400

    contact links added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,18 +7,24 @@ const Contact = () => {
       <p>Got a question or want to work together on something?</p>
       <p>Feel free to reach out.</p>
       <div className="contact-icons">
-        <img
-          src="https://cdn1.iconfinder.com/data/icons/modern-universal/32/icon-02-1024.png"
-          className="tech-icon"
-        />
-        <img
-          src="https://cdn1.iconfinder.com/data/icons/logotypes/32/github-1024.png"
-          className="tech-icon"
-        />
-        <img
-          src="https://cdn0.iconfinder.com/data/icons/typicons-2/24/social-linkedin-1024.png"
-          className="tech-icon"
-        />
+        <a href="mailto:will@willbowles.co">
+          <img
+            src="https://cdn1.iconfinder.com/data/icons/modern-universal/32/icon-02-1024.png"
+            className="tech-icon"
+          />
+        </a>
+        <a href="https://github.com/vvillbiii">
+          <img
+            src="https://cdn1.iconfinder.com/data/icons/logotypes/32/github-1024.png"
+            className="tech-icon"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/will-bowles/">
+          <img
+            src="https://cdn0.iconfinder.com/data/icons/typicons-2/24/social-linkedin-1024.png"
+            className="tech-icon"
+          />
+        </a>
       </div>
     </div>
   );
